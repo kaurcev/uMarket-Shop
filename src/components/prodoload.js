857406cbfa21b8c37905f24360812cc25709a80a
kaurcev <skaurcev2004@yandex.ru>
2024-03-16 23:40:34 +0600

    Заглушка товаров

---
+++ src/components/prodoload.js
@@ -0,0 +1,43 @@
+import './prodoload.css';
+import React from 'react';
+
+export default function Prodoload() {
+
+    return (
+      <>
+        <div className="cart load">
+            <div className='img'></div>
+        <div className="pan">
+            <div className='h4'></div>
+            <div className='h5'></div>
+            <div className='h5'></div>
+        </div>
+        </div>
+        <div className="cart load">
+        <div className='img'></div>
+        <div className="pan">
+            <div className='h4'></div>
+            <div className='h5'></div>
+            <div className='h5'></div>
+        </div>
+        </div>
+        <div className="cart load">
+        <div className='img'></div>
+        <div className="pan">
+            <div className='h4'></div>
+            <div className='h5'></div>
+            <div className='h5'></div>
+        </div>
+        </div>
+        <div className="cart load">
+        <div className='img'></div>
+        <div className="pan">
+            <div className='h4'></div>
+            <div className='h5'></div>
+            <div className='h5'></div>
+        </div>
+        </div>
+  
+      </>
+    )
+  }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import StoksPanel from '../components/stocks';
+import Prodoload from '../components/prodoload';
 
 export default function MainPage() {
   document.title = "Главная";
@@ -14,6 +15,10 @@ export default function MainPage() {
         <Header />
           <main>
             <StoksPanel />
+            <h3>Эксклюзив юМаркет Шоп</h3>
+            <div className="productpanel">
+              <Prodoload />
+            </div>
           </main>
        <Footer />
       </>
